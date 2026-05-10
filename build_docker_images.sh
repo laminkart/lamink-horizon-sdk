bf0d0a250f6035685a6d3f6212fbc60047af2381
Gustavo Rezende <g.rezendesilva@tudelft.nl>
2026-05-10 13:03:14 -0300

    update docker config

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-CURDIR=`pwd`
+set -e
+CURDIR=$(pwd)
+
+# Load pinned git commit SHAs and forward them as build-args.
+# Python package versions are in requirements.txt (copied into the images).
+set -a; source docker/versions.env; set +a
+
+VERSION_ARGS=(
+    --build-arg ARDUSUB_COMMIT
+    --build-arg ARDUPILOT_GAZEBO_COMMIT
+)
+
 docker build -t kasm-jammy:dev -f docker/dockerfile-kasm-core-jammy .
-docker build -t suave:dev --build-arg BASE_IMAGE=kasm-jammy:dev -f docker/dockerfile-suave .
-docker build -t suave-headless:dev -f docker/dockerfile-suave-headless .
-cd $CURDIR
+docker build -t suave:dev --build-arg BASE_IMAGE=kasm-jammy:dev "${VERSION_ARGS[@]}" -f docker/dockerfile-suave .
+docker build -t suave-headless:dev "${VERSION_ARGS[@]}" -f docker/dockerfile-suave-headless .
+
+cd "$CURDIR"
